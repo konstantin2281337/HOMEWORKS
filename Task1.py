146037f3249750d18c6e2002e385553a1be60968
Shevchenko Konstantin <Deltax2003@mail.ru>
2022-01-18 23:37:31 +1000

    second

--- Task1.py
+++ Task1.py
@@ -1,20 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
+s = input()
+print(s[-1]+s[1: -1]+s[0])
 
-# In[1]:
-
-
-s = [1, 2, 3, 4, 5, 6, 7]
-
-s.insert(0, s[len(s) - 1])
-s.insert(len(s), s[1])
-s.pop(len(s) - 2)
-s.pop(1)
-
-print("".join(map(str, s)))
-
-
-# In[ ]:
 
 
 
